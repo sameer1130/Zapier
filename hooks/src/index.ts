ff0cbb3fe18fc9a876a68162eb38e899db3c4918
Sameer Kamble <sameerkamble1130@gmail.com>
2024-08-01 17:21:53 -0400

    added prisma and transactions in index

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const client = new PrismaClient();
 const app = express();
+app.use(express.json());
 
 
-app.post("/hooks/catch/:userId/:zapId", (req, res)=> {
+app.post("/hooks/catch/:userId/:zapId", async(req, res)=> {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
-})
+    const body = req.body;
+
+    await client.$transaction(async tx =>{
+        const run = await client.zapRun.create({
+            data:{
+                zapId: zapId,
+                metadata: body
+            }
+        });
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+
+        res.json({
+            message: "Webhook received!!"
+        })
+        
+    })
+
+
+
+
+});
+
+app.listen(3000);
